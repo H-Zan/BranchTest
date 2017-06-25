b153669279b7dde0934d845626a4d8b1df37ca10
HuZan <huzandevil@gmail.com>
2017-06-25 11:52:32 +0800

    give tutu an age

--- app/src/main/java/com/example/huzan/branchtest/MainActivity.java
+++ app/src/main/java/com/example/huzan/branchtest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Person tutu = new Person();
+        tutu.age = 20;
     }
 }
